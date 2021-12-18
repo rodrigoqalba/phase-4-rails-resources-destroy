143692ea06c8bdd00f82a78db685861e0e5a1cfe
Rodrigo Alba <rodrigo@megabadvisors.com>
2021-12-18 18:01:10 -0400

    commit tree 84fd3dd2a45a269aa1f7e698cf7734491e7bad6f

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  resources :birds, only: [:index, :show, :create, :update]
+  # because all 5 routes are being used, we dont need to include the only option
+  resources :birds#, only: [:index, :show, :create, :update, :destroy]
   patch "/birds/:id/like", to: "birds#increment_likes"
 end
